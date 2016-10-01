a63f01471fd5bd2a431f9085ae8314fbbb9db2ef
Ian Keen <iankeen82@gmail.com>
2016-10-01 09:33:08 -0700

    limited messages coming through a MessageService to actual message and /me s - added the ability to override

--- Sources/Sugar/Services/SlackMessageService.swift
+++ Sources/Sugar/Services/SlackMessageService.swift
@@ -1,10 +1,16 @@
 import Bot
 
 open class SlackMessageService: SlackRTMEventService {
+    open var allowedSubTypes: [MessageSubType] { return [.me_message] }
+    
     public init() { }
     
     open func configureEvents(slackBot: SlackBot, webApi: WebAPI, dispatcher: SlackRTMEventDispatcher) {
         dispatcher.onEvent(message.self) { data in
+            if let subtype = data.message.subtype, !self.allowedSubTypes.contains(subtype) {
+                return
+            }
+            
             try self.messageEvent(
                 slackBot: slackBot,
                 webApi: webApi,

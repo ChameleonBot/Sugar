d0ed3f7b9932e96eaae5f243ea854a852df85d4f
Ian Keen <iankeen82@gmail.com>
2016-10-03 11:53:05 -0700

    added `Sequence.anyOrNone` and `String.orNone` for nicer pattern matching, thanks to Ryan Arana

--- Sources/Sugar/PatternMatching/Protocols/PartialPatternMatcher.swift
+++ Sources/Sugar/PatternMatching/Protocols/PartialPatternMatcher.swift
@@ -33,7 +33,16 @@ extension PartialPatternMatcher {
     public var isRequired: Bool { return true }
 }
 
-extension String: PartialPatternMatcher { }
+extension String: PartialPatternMatcher {
+    /**
+     Attempt to match this `String`s value, _or_ nothing
+     
+     - returns: A new `PartialPatternMatcher` instance to be used in pattern matching
+     */
+    var orNone: PartialPatternMatcher {
+        return [self].anyOrNone
+    }
+}
 extension Int: PartialPatternMatcher { }
 extension Double: PartialPatternMatcher { }
 extension Bool: PartialPatternMatcher { }
@@ -46,5 +55,19 @@ extension Sequence where Iterator.Element: PartialPatternMatcher {
      */
     public var any: PartialPatternMatcher {
         return SequencePatternMatcher(input: self)
+    }
+}
+
+extension Sequence where Iterator.Element: PartialPatternMatcher, Iterator.Element == String {
+    /**
+     Converts the receiver into a `PartialPatternMatcher` test that can be used in pattern matching
+     It will attempt to match only items in the seqeunce, _or_ nothing
+     
+     - returns: A new `PartialPatternMatcher` instance to be used in pattern matching
+     */
+    public var anyOrNone: PartialPatternMatcher {
+        var values = Set<String>(self)
+        values.remove("")
+        return SequencePatternMatcher(input: values + [""])
     }
 }
